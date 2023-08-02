a78fdb1953d8fba486c6564782677acbf0488746
downYotube2548 <maneeohooho3790@gmail.com>
2023-08-02 20:25:56 +0700

    Reaction Testing

--- src/main/java/com/dev/damagehandler/reaction/reactions/Overloaded.java
+++ src/main/java/com/dev/damagehandler/reaction/reactions/Overloaded.java
@@ -1,12 +1,15 @@
 package com.dev.damagehandler.reaction.reactions;
 
+import com.dev.damagehandler.DamageHandler;
 import com.dev.damagehandler.reaction.ElementalReaction;
 import com.dev.damagehandler.utils.ConfigLoader;
 import io.lumine.mythic.lib.damage.DamagePacket;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Overloaded extends ElementalReaction {
     public Overloaded() {
@@ -14,7 +17,15 @@ public class Overloaded extends ElementalReaction {
     }
 
     @Override
-    public void trigger(DamagePacket damage, LivingEntity entity, Entity damager) {
-        Bukkit.broadcastMessage(ChatColor.RED+"Overloaded");
+    public void trigger(DamagePacket damage, double gauge_unit, String decay_rate, LivingEntity entity, Entity damager) {
+        if (damage.getElement() == null) return;
+        getAuraData(entity.getUniqueId()).removeAura(getTrigger());
+        double final_gauge_unit = gauge_unit * ConfigLoader.getGaugeUnitTax(getId());
+        getAuraData(entity.getUniqueId()).reduceAura(getAura(), final_gauge_unit);
+
+        displayIndicator(getDisplay(), entity);
+        displayIndicator("&c100", entity);
+
+        damage(100, damager, entity);
     }
 }
--- src/main/java/com/dev/damagehandler/reaction/reactions/ReverseOverloaded.java
+++ src/main/java/com/dev/damagehandler/reaction/reactions/ReverseOverloaded.java
@@ -1,10 +1,11 @@
 package com.dev.damagehandler.reaction.reactions;
 
+import com.dev.damagehandler.DamageHandler;
 import com.dev.damagehandler.reaction.ElementalReaction;
 import com.dev.damagehandler.utils.ConfigLoader;
+import com.dev.damagehandler.utils.Utils;
 import io.lumine.mythic.lib.damage.DamagePacket;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
@@ -14,7 +15,15 @@ public class ReverseOverloaded extends ElementalReaction {
     }
 
     @Override
-    public void trigger(DamagePacket damage, LivingEntity entity, Entity damager) {
-        Bukkit.broadcastMessage(ChatColor.GOLD+"Reverse Overloaded "+(damager != null));
+    public void trigger(DamagePacket damage, double gauge_unit, String decay_rate, LivingEntity entity, Entity damager) {
+        if (damage.getElement() == null) return;
+        DamageHandler.getAura().getAura(entity.getUniqueId()).removeAura(getTrigger());
+        double final_gauge_unit = gauge_unit * ConfigLoader.getGaugeUnitTax(getId());
+        DamageHandler.getAura().getAura(entity.getUniqueId()).reduceAura(getAura(), final_gauge_unit);
+
+        displayIndicator(getDisplay(), entity);
+        displayIndicator("&c100", entity);
+
+        damage(100, damager, entity);
     }
 }
