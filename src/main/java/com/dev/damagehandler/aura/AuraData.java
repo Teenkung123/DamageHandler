06757419eee1ff2b2140180be68baeea2bc405cd
downYotube2548 <maneeohooho3790@gmail.com>
2023-08-02 20:23:44 +0700

    Special Aura

--- src/main/java/com/dev/damagehandler/aura/AuraData.java
+++ src/main/java/com/dev/damagehandler/aura/AuraData.java
@@ -16,13 +16,13 @@ import static com.dev.damagehandler.aura.Aura.entityAura;
  */
 public class AuraData {
     private final UUID uuid;
-    private final Map<String, Long> mapAura;
+    private final Map<String, AuraGauge> mapAura;
     public AuraData(UUID uuid) {
         this.uuid = uuid;
         this.mapAura = new HashMap<>();
     }
 
-    public Map<String, Long> getMapAura() {
+    public Map<String, AuraGauge> getMapAura() {
         return mapAura;
     }
 
@@ -32,55 +32,64 @@ public class AuraData {
      */
     public String getAuraIcon() {
         StringBuilder sb = new StringBuilder();
-        for (String elementID : mapAura.keySet()) {
+        for (String auraID : mapAura.keySet()) {
 
-            Element element = MythicLib.plugin.getElements().get(elementID);
-            if (element == null) continue;
+            String auraIcon;
+            Element element = MythicLib.plugin.getElements().get(auraID);
+            if (element != null) {
+                auraIcon = element.getColor()+element.getLoreIcon();
+            } else {
+                auraIcon = ConfigLoader.getSpecialAuraColor(auraID)+ConfigLoader.getSpecialAuraIcon(auraID);
+            }
 
-            sb.append(Utils.colorize(element.getColor()+element.getLoreIcon()+"&r"));
+            sb.append(Utils.colorize(auraIcon+"&r"));
         }
         return sb.toString();
     }
 
-    public void addAura(String element, double gauge_unit) {
-        addAura(element, (long)(Math.floor(ConfigLoader.getAuraTime() * gauge_unit)));
-    }
-
     public void reduceAura(String element, double gauge_unit) {
-        reduceAura(element, (long)(Math.floor(ConfigLoader.getAuraTime() * gauge_unit)));
-    }
-
-    protected void addAura(String element, long duration) {
-        if (MythicLib.plugin.getElements().get(element) == null) return;
-        if (this.mapAura.containsKey(element) && duration < this.mapAura.get(element)) return;
-
-        this.mapAura.put(element, duration);
-        if (!entityAura.containsKey(this.uuid)) entityAura.put(this.uuid, this);
-    }
-
-    protected void setAura(String element, long duration) {
-        if (MythicLib.plugin.getElements().get(element) == null) return;
-        this.mapAura.put(element, duration);
-        if (!entityAura.containsKey(this.uuid)) entityAura.put(this.uuid, this);
-    }
-
-    protected void removeAura(String element) {
-        if (MythicLib.plugin.getElements().get(element) == null) return;
         if (!this.mapAura.containsKey(element)) return;
+        reduceAura(element, (long)(Math.floor(gauge_unit * ConfigLoader.getDecayRate(this.mapAura.get(element).getDecayRate()))));
+    }
 
-        this.mapAura.remove(element);
+    public void addAura(String aura, double gauge_unit, String decay_rate) {
+        if (MythicLib.plugin.getElements().get(aura) == null && ConfigLoader.getSpecialAuraIcon(aura) == null) return;
+
+
+        if (!this.mapAura.containsKey(aura)) {
+            this.mapAura.put(aura, new AuraGauge((long) Math.floor(gauge_unit * ConfigLoader.getDecayRate(decay_rate)), decay_rate));
+        } else {
+            String old_decay_rate = this.mapAura.get(aura).getDecayRate();
+            this.mapAura.put(aura, new AuraGauge((long) Math.floor(gauge_unit * ConfigLoader.getDecayRate(old_decay_rate)), old_decay_rate));
+        }
+
+
+        if (!entityAura.containsKey(this.uuid)) entityAura.put(this.uuid, this);
+    }
+
+    protected void setAura(String aura, long duration, String decay_rate) {
+        if (MythicLib.plugin.getElements().get(aura) == null && ConfigLoader.getSpecialAuraIcon(aura) == null) return;
+        this.mapAura.put(aura, new AuraGauge(duration, decay_rate));
+        if (!entityAura.containsKey(this.uuid)) entityAura.put(this.uuid, this);
+    }
+
+    public void removeAura(String aura) {
+        if (MythicLib.plugin.getElements().get(aura) == null && ConfigLoader.getSpecialAuraIcon(aura) == null) return;
+        if (!this.mapAura.containsKey(aura)) return;
+
+        this.mapAura.remove(aura);
 
         if (mapAura.isEmpty()) entityAura.remove(this.uuid);
     }
 
-    protected void reduceAura(String element, long duration) {
-        if (MythicLib.plugin.getElements().get(element) == null) return;
-        if (!this.mapAura.containsKey(element)) return;
+    protected void reduceAura(String aura, long duration) {
+        if (MythicLib.plugin.getElements().get(aura) == null && ConfigLoader.getSpecialAuraIcon(aura) == null) return;
+        if (!this.mapAura.containsKey(aura)) return;
 
-        if (this.mapAura.get(element) <= duration) {
-            removeAura(element);
+        if (this.mapAura.get(aura).getDuration() <= duration) {
+            removeAura(aura);
         } else {
-            setAura(element, this.mapAura.get(element) - duration);
+            setAura(aura, this.mapAura.get(aura).getDuration() - duration, this.mapAura.get(aura).getDecayRate());
         }
     }
 }
